cdb85075522c9b5d6ff23ee0246e824d6da27e5d
gkiltava <gkiltava@gmail.com>
2025-11-03 23:13:42 +0400

    feat: Enable NGRX strict runtime checks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,7 +51,15 @@ const routes: Routes = [
         MatListModule,
         MatToolbarModule,
         AuthModule.forRoot(),
-        StoreModule.forRoot(reducers, {}),
+        StoreModule.forRoot(reducers, {
+          // metaReducers,
+          runtimeChecks: {
+            strictStateImmutability: true,
+            strictActionImmutability: true,
+            strictStateSerializability: true,
+            strictActionSerializability: true,
+          }
+        }),
         StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
       EffectsModule.forRoot([]),
       StoreRouterConnectingModule.forRoot({
